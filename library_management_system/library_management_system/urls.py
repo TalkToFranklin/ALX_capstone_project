618c0053f55fd4335dfaabd2bff7715c14816845
Franklin Akinboboye <boyefranklin@gmail.com>
2024-10-19 06:56:50 -0700

    Defined models, created Serializers, created Views, defined URLs

--- library_management_system/library_management_system/urls.py
+++ library_management_system/library_management_system/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from api.views import BookViewSet, LibraryUserViewSet, TransactionViewSet
 
 router = DefaultRouter()
